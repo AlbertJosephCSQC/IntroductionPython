4022cae234dcc33ae2660e4c51311de995a08329
jason <jasonithiel125@gmail.com>
2026-07-19 20:16:15 +0530

    new

--- Strings/String length/len_function.py
+++ Strings/String length/len_function.py
@@ -3,6 +3,6 @@ It is a really long string
 triple-quoted strings are used
 to define multi-line strings
 """
-index_to_slice = ???  # Get the index up to which you will slice.
+index_to_slice = ???  # Get te index up to which you will slice.
 first_half = ???  # Get the slice of the phrase.
 print(first_half)
